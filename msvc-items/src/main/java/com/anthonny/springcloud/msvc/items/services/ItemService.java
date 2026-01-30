be2467767cdea09c6ecfbdcb4e8e5899eff39d0e
Anthonny <anthonny17junio@gmail.com>
2026-01-30 14:55:15 +0100

    MsvcItems: Model classes and ItemService creation

---
+++ msvc-items/src/main/java/com/anthonny/springcloud/msvc/items/services/ItemService.java
@@ -0,0 +1,13 @@
+package com.anthonny.springcloud.msvc.items.services;
+
+import com.anthonny.springcloud.msvc.items.models.Item;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemService {
+
+    List<Item> findAll();
+
+    Optional<Item> findById(Long id);
+}

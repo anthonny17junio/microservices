607f17db5c8dd407518d124d5f7c04ca3065771c
Anthonny <anthonny17junio@gmail.com>
2026-02-07 20:04:29 +0100

    EurekaServer: New project creation

---
+++ eureka-server/src/test/java/com/anthonny/springcloud/eureka/server/app/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonny.springcloud.eureka.server.app.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
